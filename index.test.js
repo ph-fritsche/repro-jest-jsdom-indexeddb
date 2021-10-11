0bd9819d0ab4175e39e446304d9c6353cb3e3aec
Philipp Fritsche <ph.fritsche@gmail.com>
2021-10-11 16:37:05 +0000

    confirm order of execution

--- index.test.js
+++ index.test.js
@@ -32,3 +32,30 @@ test("walk cursor", async () => {
         cursor = await cursor.continue()
     }
 })
+
+test("transaction is `active` on next tick", async () => {
+    const db = await openDB("test", undefined, {
+        upgrade(_db) {
+            _db.createObjectStore("foo")
+        }
+    })
+
+    const tx = db.transaction("foo", "readwrite")
+    expect(tx._state).toBe('active')
+
+    await new Promise(r => process.nextTick(r))
+
+    expect(tx._state).toBe('active')
+})
+
+test('nextTick comes before setImmediate', async () => {
+    const events = []
+
+    setImmediate(() => events.push('Immediate'))
+    process.nextTick(() => events.push('nextTick'))
+    events.push('sameTick')
+
+    await new Promise(r => setTimeout(r, 10))
+
+    expect(events).toEqual(['sameTick', 'nextTick', 'Immediate'])
+})
